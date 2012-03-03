aa52697e322b95488daee895c02660f14cc64b58
Dan Lidral-Porter <dlp@aperiodic.org>
2012-03-02 17:55:15 -0800

    Much better uninstall script.
    For each file installed with install.sh, the file will always be
    restored from the backup file if it exists. If not, then an ls -lh on
    the file in question will be displayed, after which the user will asked
    to decide whether to delete the file. If the -y flag is passed to the
    script, then files without backups will be deleted without confirmation.

--- uninstall.sh
+++ uninstall.sh
@@ -1,35 +1,65 @@
 #!/bin/bash
 
-function restore {
-  if [[ ! -e ~/$1.bkp ]]; then
-    echo "No ~/$1.bkp file to revert from, skipping ~/$1"
-  else
+function revert_or_rm {
+  if [[ -e ~/$1.bkp ]]; then
     mv ~/$1.bkp ~/$1
     echo "Reverted $1"
+  else
+    if [ $NUKE ]; then
+      rm ~/$1
+      echo "Deleted ~/$1"
+    else
+      ls -lh ~/$1
+      printf "Delete '~/$1'? [y/n]: "
+      read _del_confirm
+      if [[ $_del_confirm -eq "y" ]]; then
+        rm ~/$1
+      fi
+      # this might not be necessary; i don't really know bash variable scoping
+      unset _del_confirm
+    fi
   fi
 }
 
-echo "All files installed by install.sh will be restored from their backups, if"
-echo "possible. Any files without a backup will not be modified"
-printf "Continue? [y/n]: "
-read confirm
+ARG_COUNT=$#
 
-if [[ ! $confirm =~ "y" ]]; then
+while getopts "y" opt; do
+  case "$opt" in
+    y) NUKE=true; ARG_COUNT=$((ARG_COUNT - 1));;
+    ?) echo "unrecognized option '$opt'!"; exit 1;;
+  esac
+done
+
+if [ $NUKE ]; then
+  echo "I say we take off and nuke the entire site from orbit."
+  echo "    - Ellen Ripley"
+fi
+
+if [ ! $NUKE ]; then
+  echo "All files installed by install.sh will be either restored from backups or"
+  echo "removed. You will have a chance to review any files without a backup and elect"
+  echo "to preserve them. To skip this process and automatically delete files without"
+  echo "backups, rerun this script and pass the '-y' flag."
+  printf "Continue? [y/n]: "
+  read confirm
+fi
+
+if [[ ! $confirm -eq "y" && ! $NUKE ]]; then
   exit 1
 fi
 
 set -e
 
-if [ $# -eq 0 ]; then
-  restore .bashrc
-  restore .zshrc
-  restore .inputrc
-  restore .tmux.conf
+if [ $ARG_COUNT -eq 0 ]; then
+  revert_or_rm .bashrc
+  revert_or_rm .zshrc
+  revert_or_rm .inputrc
+  revert_or_rm .tmux.conf
 
-  restore .vim
-  restore .vimrc
+  revert_or_rm .vim
+  revert_or_rm .vimrc
 else
   for f in $@; do
-    restore $f
+    revert_or_rm $f
   done
 fi
