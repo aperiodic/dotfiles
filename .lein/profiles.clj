4b1642f3952e450da485d8595edc33418592c7ea
aperiodic <dlp@aperiodic.org>
2013-10-30 16:23:49 -0700

    Add ns tools, clj-stacktrace to lein user profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-difftest "1.3.7"]
-                  [lein-swank "1.4.4"]
-                  [lein-midje "3.0.0"]]}}
+                  [lein-midje "3.0.0"]]
+        :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                       [clj-stacktrace "0.2.5"]]}}
