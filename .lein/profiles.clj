2539cc11d21b3a2cbc4317a203d20eb423efbb76
aperiodic <dlp@aperiodic.org>
2014-05-03 20:27:01 -0700

    Update .lein/profiles.clj
    Update the clj-stacktrace dependency and add its injection.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,9 @@
 {:user {:plugins [[lein-difftest "1.3.7"]
                   [lein-midje "3.0.0"]]
         :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                       [clj-stacktrace "0.2.5"]]}}
+                       [clj-stacktrace "0.2.7"]]
+        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           'pst)]
+                       (alter-var-root orig (constantly (deref new))))]}}
