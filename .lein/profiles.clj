0f49355cf90a6da06d692d26f40f3cd36e0c1e2c
Dan Lidral-Porter <dan.lidral-porter@puppetlabs.com>
2014-05-20 18:16:15 -0700

    Update lein-difftest to 2.0.0, rm clj-stacktrace

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[lein-difftest "1.3.7"]
+{:user {:plugins [[lein-difftest "2.0.0"]
                   [lein-midje "3.0.0"]]
         :dependencies [[org.clojure/tools.namespace "0.2.4"]
                        [clj-stacktrace "0.2.7"]]
