f69bc4d9443d0166d9fe6f440674baccbc614e6f
aperiodic <dlp@aperiodic.org>
2014-06-10 20:35:56 -0700

    Change xmonad terminal to xfce-terminal

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -14,7 +14,7 @@ import qualified Data.Map        as M
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal      = "urxvt"
+myTerminal      = "xfce4-terminal"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse = True
