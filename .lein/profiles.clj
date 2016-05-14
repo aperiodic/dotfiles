f26fdf6130a8e789a593427026c78b1be6f77fb2
aperiodic <dlp@aperiodic.org>
2016-05-14 02:18:14 -0700

    Upgrade clj-stacktrace

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[lein-difftest "2.0.0"]
                   [lein-midje "3.0.0"]]
         :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                       [clj-stacktrace "0.2.7"]]
+                       [clj-stacktrace "0.2.8"]]
         :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
                                             'print-cause-trace)
                            new (ns-resolve (doto 'clj-stacktrace.repl require)
