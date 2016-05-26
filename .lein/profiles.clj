fe29856c3f332d884d2abe000410168627387745
Dan Lidral-Porter <dan.lidral-porter@puppetlabs.com>
2016-05-26 11:33:26 -0700

    Update deps in profiles.clj & add low default Xmx

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,9 @@
 {:user {:plugins [[lein-difftest "2.0.0"]
-                  [lein-midje "3.0.0"]]
+                  [lein-midje "3.0.0"]
+                  [lein-pprint "1.1.2"]]
         :dependencies [[org.clojure/tools.namespace "0.2.4"]
                        [clj-stacktrace "0.2.8"]]
+        :jvm-opts ["-Xmx256m"]
         :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
                                             'print-cause-trace)
                            new (ns-resolve (doto 'clj-stacktrace.repl require)
