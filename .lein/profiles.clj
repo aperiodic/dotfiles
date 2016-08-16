b105cde4dd84f43c07d19d6607b89a957ad19908
Dan Lidral-Porter <dan.lidral-porter@puppetlabs.com>
2016-08-16 14:53:22 -0700

    Remove lein user profile's tools.namespace dep

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,7 @@
 {:user {:plugins [[lein-difftest "2.0.0"]
                   [lein-midje "3.0.0"]
                   [lein-pprint "1.1.2"]]
-        :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                       [clj-stacktrace "0.2.8"]]
+        :dependencies [[clj-stacktrace "0.2.8"]]
         :jvm-opts ["-Xmx256m"]
         :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
                                             'print-cause-trace)
