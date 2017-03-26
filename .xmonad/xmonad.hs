14b7438f5255e1da3b36c4b52637601c5650120e
aperiodic <dlp@aperiodic.org>
2017-03-26 12:44:12 -0700

    (xmonad) Refactor xmonad.hs
    Trim down my xmonad.hs significantly by removing a lot of variables that
    had no real reason to be variables in favor of just passing the values
    directly to the desktopConfig constructor in main, and also removing
    a lot of the explanatory comments.
    
    Now there are five sections:
      * key bindings
      * mouse bindings
      * the management hook definition (to ignore some applications)
      * Xmobar configuration
      * the main fn with xmonad invocation & all other configuration

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -16,45 +16,6 @@ import System.Exit
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
--- The preferred terminal program, which is used in a binding below and by
--- certain contrib modules.
---
-myTerminal      = "xfce4-terminal --hide-menubar"
-
--- Whether focus follows the mouse pointer.
-myFocusFollowsMouse = True
-
--- Whether clicking on a window to focus also passes the click to the window
-myClickJustFocuses :: Bool
-myClickJustFocuses = False
-
--- Width of the window border in pixels.
---
-myBorderWidth   = 1
-
--- modMask lets you specify which modkey you want to use. The default
--- is mod1Mask ("left alt").  You may also consider using mod3Mask
--- ("right alt"), which does not conflict with emacs keybindings. The
--- "windows key" is usually mod4Mask.
---
-myModMask       = mod1Mask
-
--- The default number of workspaces (virtual screens) and their names.
--- By default we use numeric strings, but any string may be used as a
--- workspace name. The number of workspaces is determined by the length
--- of this list.
---
--- A tagging example:
---
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
---
-myWorkspaces    = ["0","1","2","3","4","5","6","7","8","9"]
-
--- Border colors for unfocused and focused windows, respectively.
---
-myNormalBorderColor  = "#333333"
-myFocusedBorderColor = "#cb4b16"
-
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
@@ -75,31 +36,19 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     --  Reset the layouts on the current workspace to default
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
 
-    -- Move focus to the next window
+    -- Move focus
     , ((modm,               xK_Tab   ), windows W.focusDown)
-
-    -- Move focus to the next window
     , ((modm,               xK_j     ), windows W.focusDown)
-
-    -- Move focus to the previous window
     , ((modm,               xK_k     ), windows W.focusUp  )
-
-    -- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster  )
 
-    -- Swap the focused window and the master window
+    -- Swap windows
     , ((modm,               xK_Return), windows W.swapMaster)
-
-    -- Swap the focused window with the next window
     , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-
-    -- Swap the focused window with the previous window
     , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
 
     -- move right edge of column left or right (or left edge if last column)
     , ((modm,               xK_h     ), withFocused $ \w -> sendMessage $ VC.Embiggen (-dw) 0 w)
-
-    -- Expand the master area
     , ((modm,               xK_l     ), withFocused $ \w -> sendMessage $ VC.Embiggen dw 0 w)
 
     -- move bottom edge of window up or down (or top edge if last window in column)
@@ -117,8 +66,6 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm,               xK_r     ), withFocused $ windows . W.sink)
 
     -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
     , ((modm              , xK_s     ), sendMessage ToggleStruts)
 
     -- Quit xmonad
@@ -145,14 +92,13 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        | (key, sc) <- zip [xK_o, xK_e, xK_u] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
   where
     dw = 0.02
 
-
 ------------------------------------------------------------------------
--- Mouse bindings: default actions bound to mouse events
+-- Mouse bindings
 --
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 
@@ -175,22 +121,6 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
     dw = 0.005
 
 ------------------------------------------------------------------------
--- Layouts:
-
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
---
-myLayout = tiled ||| twocol ||| Full
-  where
-    tiled = VC.varial
-    twocol = Limit.limitWindows 2 $ VC.varial
-
-------------------------------------------------------------------------
 -- Window rules:
 
 -- Execute arbitrary actions and WindowSet manipulations when managing
@@ -205,7 +135,7 @@ myLayout = tiled ||| twocol ||| Full
 -- To match on the WM_NAME, you can use 'title' in the same way that
 -- 'className' and 'resource' are used below.
 --
-myManageHook = composeAll
+bypassSomeApps = composeAll
     [ className =? "MPlayer"          --> doFloat
     , className =? "Gimp"             --> doFloat
     , className =? "hl_linux"         --> doFloat
@@ -214,32 +144,10 @@ myManageHook = composeAll
     , resource  =? "kdesktop"         --> doIgnore ]
 
 ------------------------------------------------------------------------
--- Event handling
-
--- * EwmhDesktops users should change this to ewmhDesktopsEventHook
---
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
---
-myEventHook = hintsEventHook <+> fullscreenEventHook
-
------------------------------------------------------------------------
--- Startup hook
-
--- Perform an arbitrary action each time xmonad starts or is restarted
--- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
--- per-workspace layout choices.
---
--- By default, do nothing.
-myStartupHook = setWMName "LG3D"
-
-------------------------------------------------------------------------
 -- Xmobar
 --
-myBar = "xmobar"
-
 -- Configure the workspaces pretty printer
+--
 myPP = xmobarPP { ppCurrent = xmobarColor "#fdf6e3" "" . wrap "[" "]"
                 , ppLayout  = (\x -> case x of
                                           "Varial Columns"         -> "|+"
@@ -250,35 +158,36 @@ myPP = xmobarPP { ppCurrent = xmobarColor "#fdf6e3" "" . wrap "[" "]"
                 }
 
 ------------------------------------------------------------------------
--- Now run xmonad with all the defaults we set up.
-
--- Run xmonad with the settings you specify. No need to modify this.
+-- Main
 --
 main :: IO ()
 main = do
-  h <- spawnPipe myBar
+  h <- spawnPipe "xmobar"
   xmonad $
     docks
     desktopConfig
     {
-    -- simple stuff
-      terminal           = myTerminal
-    , focusFollowsMouse  = myFocusFollowsMouse
-    , clickJustFocuses   = myClickJustFocuses
-    , borderWidth        = myBorderWidth
-    , modMask            = myModMask
-    , workspaces         = myWorkspaces
-    , normalBorderColor  = myNormalBorderColor
-    , focusedBorderColor = myFocusedBorderColor
+    -- properties
+      terminal           = "xfce4-terminal --hide-menubar"
+    , focusFollowsMouse  = True
+    , clickJustFocuses   = False
+    , borderWidth        = 1
+    , modMask            = mod1Mask
+    , workspaces         = map show [0..9]
+    , normalBorderColor  = "#333333"
+    , focusedBorderColor = "#cb4b16"
 
-    -- key bindings
+    -- key & mouse bindings
     , keys               = myKeys
     , mouseBindings      = myMouseBindings
 
-    -- hooks layouts
-    , layoutHook         = desktopLayoutModifiers $ myLayout
-    , manageHook         = myManageHook
-    , handleEventHook    = myEventHook
+    -- behavorial costumization
+    , layoutHook         = desktopLayoutModifiers $ tiled ||| twocol ||| Full
+    , manageHook         = bypassSomeApps
+    , handleEventHook    = hintsEventHook <+> fullscreenEventHook
     , logHook            = dynamicLogWithPP $ myPP { ppOutput = hPutStrLn h }
-    , startupHook        = myStartupHook
+    , startupHook        = setWMName "LG3D"
   }
+  where
+    tiled = VC.varial
+    twocol = Limit.limitWindows 2 $ VC.varial
