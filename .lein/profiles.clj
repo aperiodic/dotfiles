05a69193171ad95730dae21a4c7709b7230cd531
aperiodic <dlp@aperiodic.org>
2017-04-20 08:54:11 -0700

    More exclusions for lein-ancient plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[lein-ancient "0.6.10" :exclusions [org.apache.httpcomponents/httpclient]]
+{:user {:plugins [[lein-ancient "0.6.10" :exclusions [org.apache.httpcomponents/httpclient com.fasterxml.jackson.core/jackson-core]]
                   [lein-difftest "2.0.0"]
                   [lein-midje "3.0.0"]
                   [lein-pprint "1.1.2"]]
